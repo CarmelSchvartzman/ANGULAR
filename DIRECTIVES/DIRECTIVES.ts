7f50d84e34db4ca5b86b4fe5bd04bb75748c1652
Carmel Shvartzman <carmel.aspnet@gmail.com>
2019-08-28 08:24:27 +0300

    Update DIRECTIVES.ts

--- DIRECTIVES/DIRECTIVES.ts
+++ DIRECTIVES/DIRECTIVES.ts
@@ -1,2 +1,27 @@
-A DIRECTIVE DEFINES BEHAVIOUR TO A DOM ELEMENT:
+/////////   A DIRECTIVE DEFINES BEHAVIOUR TO A DOM ELEMENT:
 
+import {Directive, Input} from "@angular/core";
+
+@Directive({
+    selector: 'hero',
+})
+export class Hero {
+
+    @Input()
+    id: number;
+
+    @Input()
+    name:string;
+}
+
+
+/////////////  USE :
+
+<heroes>
+    <hero id="1" name="Superman"></hero>
+    <hero id="2" name="Batman"></hero>
+    <hero id="3" name="Batgirl"></hero>
+    <hero id="3" name="Robin"></hero>
+    <hero id="4" name="Flash"></hero>
+    <hero id="5" name="Green Lantern"></hero>
+</heroes>
